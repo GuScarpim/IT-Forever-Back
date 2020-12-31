7b93cecffd49338fd70ab0f69e3705576bf09e9e
guscarpim <gustavoscarpim@gmail.com>
2020-12-31 13:13:13 -0300

    Deploy node heroku

--- src/Database/db.js
+++ src/Database/db.js
@@ -1,19 +1,19 @@
 async function connect() {
-  if (global.connection && global.connection.state !== 'disconnected') {
-    return global.connection;
-  }
+  // if (global.connection && global.connection.state !== 'disconnected') {
+  //   return global.connection;
+  // }
 
   const mysql = require("mysql2/promise");
   const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
+    port: process.env.PORTBD,
     database: 'foreverit'
   })
   console.log('Conectou');
-  global.connection = connection;
+  // global.connection = connection;
   return connection;
 }
 
 module.exports = connect;
-// port: process.env.PORTBD,

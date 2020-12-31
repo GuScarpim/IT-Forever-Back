f11ae218ed1c7fc44684067096013103523a4265
guscarpim <gustavoscarpim@gmail.com>
2020-12-31 02:18:45 -0300

    Agora vai

--- src/Routes/UserRoute.js
+++ src/Routes/UserRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const verifyJWT = require('../Token/jwt');
 const controller = require('../Controller/UserController');
 
-router.get('/profiles', verifyJWT, controller.getUsers);
+router.get('/profiles', controller.getUsers);
 router.post('/login', controller.login);
 router.post('/logout', controller.logout);
 

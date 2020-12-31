36dc1f0c3f4c76cd52ac7920da97fb8a1d4a15da
guscarpim <gustavoscarpim@gmail.com>
2020-12-31 14:10:33 -0300

    Trocando env

--- src/Token/jwt.js
+++ src/Token/jwt.js
@@ -5,7 +5,7 @@ function verifyJWT(req, res, next) {
   const token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
   if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
 
-  jwt.verify(token, process.env.SECRET, function (err, decoded) {
+  jwt.verify(token, 'sempreIT', function (err, decoded) {
     if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
     req.userId = decoded.id;
     next();

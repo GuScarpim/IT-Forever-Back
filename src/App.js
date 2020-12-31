dd8d2ab5c2429b70071e1f6210097514560b0469
guscarpim <gustavoscarpim@gmail.com>
2020-12-31 02:57:46 -0300

    rota 3000

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@
   app.use('/api/v1/user/', ProdutosRoute);
   app.use('/api/v1/profile/', UserRoute);
 
-  const port = process.env.PORT || 3030;
+  const port = process.env.PORT || 3000;
 
   app.listen(port);
   console.log('conectado a porta ' + port);

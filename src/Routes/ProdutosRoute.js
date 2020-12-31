a96646954e23c5a3559fd8f477457de30950249c
guscarpim <gustavoscarpim@gmail.com>
2020-12-31 14:17:21 -0300

    Proteção

--- src/Routes/ProdutosRoute.js
+++ src/Routes/ProdutosRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const verifyJWT = require('../Token/jwt');
 const controller = require('../Controller/ProdutosController');
 
-router.get('/', controller.get);
+router.get('/', verifyJWT, controller.get);
 router.get('/:id', verifyJWT, controller.getId);
 router.post('/', verifyJWT, controller.post);
 router.put('/:id', verifyJWT, controller.put);
